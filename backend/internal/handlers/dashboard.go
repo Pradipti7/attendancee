89ea85be320076ddcec25c6a235470bd723a390d
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-09 15:25:13 +0545

    add: Dashboard handler added to backend

--- backend/internal/handlers/dashboard.go
+++ backend/internal/handlers/dashboard.go
@@ -1,1 +1,51 @@
 package handlers
+
+import (
+	"backend/internal/database"
+	"encoding/json"
+	"net/http"
+)
+
+type DashboardData struct {
+	TotalStudents int `json:"total_students"`
+	PresentToday  int `json:"present_today"`
+	AbsentToday   int `json:"absent_today"`
+	TotalClasses  int `json:"total_classes"`
+}
+
+func GetDashboardData(w http.ResponseWriter, r *http.Request) {
+	var data DashboardData
+
+	// for total students
+	err := database.DB.QueryRow(`
+	SELECT COUNT (*) FROM students
+	`).Scan(&data.TotalStudents)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// for present students
+	err = database.DB.QueryRow(`
+	SELECT COUNT (*)
+	FROM students
+	WHERE is_present =true
+	`).Scan(&data.PresentToday)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// for absent students
+	err = database.DB.QueryRow(`
+SELECT COUNT(*)
+FROM students
+WHERE is_present-false`).Scan(&data.PresentToday)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+
+}
