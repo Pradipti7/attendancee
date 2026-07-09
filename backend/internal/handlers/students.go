e33e2e2ed1379eff49048849fb0eeaa66312187a
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-09 16:02:42 +0545

    add: students handler added to backend

--- backend/internal/handlers/students.go
+++ backend/internal/handlers/students.go
@@ -1,1 +1,21 @@
 package handlers
+
+import (
+	"net/http"
+
+	"backend/internal/database"
+)
+
+// get all students
+func GetStudents(w http.ResponseWriter, r *http.Request) {
+	rows, err := database.DB.Query(`
+	SELECT 
+	id, name, class_name, email, address, number, is_present
+	FROM students
+	ORDER BY id; 
+	`)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
